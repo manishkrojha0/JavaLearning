d671db4168f4723ed396383824285b7aa7252e17
Manish Kumar Ojha <manishkrojha0@gmail.com>
2021-10-03 06:18:08 +0530

    added files

--- DemoApp/src/main/java/com/learningServlet/AddServlet.java
+++ DemoApp/src/main/java/com/learningServlet/AddServlet.java
@@ -12,7 +12,7 @@ public class AddServlet extends HttpServlet {
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
 		int i=Integer.parseInt(req.getParameter("num1"));
-		int j=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
 		int k= i+j;
 		
 		PrintWriter out= res.getWriter();
--- DemoApp/src/main/java/com/learningServlet/SqServlet.java
+++ DemoApp/src/main/java/com/learningServlet/SqServlet.java
@@ -1,5 +1,26 @@
 package com.learningServlet;
 
-public class SqServlet {
 
+
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest req,HttpServletResponse res)
+    {
+    	
+		try {
+			PrintWriter out = res.getWriter();
+			out.println("the product of two no is: ");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
 }
